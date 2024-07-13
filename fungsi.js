5092d1c2410262de3bb9827a9950e87c226f216f
Arfan Pria Pamungkas <arfanpp04@gmail.com>
2024-07-13 18:34:13 +0700

    tambah

---
+++ fungsi.js
@@ -0,0 +1,13 @@
+const add = (a, b) => a + b;
+console.log(add(2,3))
+
+const subtract = (a,b) => a - b;
+console.log(subtract(5,2));
+
+const multiplyNumber = (a , b) => a * b;
+console.log(multiplyNumber(3,3));
+
+const divide = (a,b) => a / b;
+console.log(divide(10,2))
+
+console.log("belajar bersama sepuh gading")
